8aea2baf1f23b4d203242078f4433ff26e4e1a4e
hemantkishore7 <hemantkishore98@gmail.com>
2023-06-17 09:31:30 +0530

    package installed and create auth

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors"
+import dotenv from "dotenv"
+import multer from "multer"
+import helmet from "helmet"
+import morgan from "morgan";
+import path from "path";
+import {fileURLToPath} from "url"
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js"
+
+
+// Configration
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(cors());
+app.use("/assets",express.static(path.join(__dirname,"public/assets")))
+
+
+// File storage
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,"public/assets");
+    },
+   filename: function(req,file,cb){
+    cb(null,file.originalname)
+   } 
+})
+const upload = multer({storage})
+
+//Routes with Files
+app.post("/auth/register",upload.single("picture"),register)
+
+//Routers
+app.use("/auth",authRoutes)
+
+// Mongoose setup 
+const port = process.env.PORT || 6000;
+mongoose.connect(process.env.DB,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    app.listen(port,()=>console.log(`Server connected on ${port}`))
+}).catch((error)=> console.log(`${error} did not connect DB`))
